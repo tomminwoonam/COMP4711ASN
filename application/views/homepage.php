c91dbbf73db4b90e890b18359786748252bc7e0b
ashayegh <shayegh.software@gmail.com>
2017-02-13 01:01:52 -0800

    Improved panda icon, improved homepage looks

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -5,21 +5,15 @@
             <h4>Parts on hand</h4>
             <h3 class="homedashvalue">{totparts}</h3>
         </div>
-        <div class="col-sm-2">
-        </div>
         <div class="col-sm-5 bluebox">
             <h4>Assembled bots</h4>
             <h3 class="homedashvalue">{totbots}</h3>
         </div>
     </div>
     <div class="col-sm-12">
-    </div>
-    <div class="col-sm-12">
         <div class="col-sm-5 greenbox">
             <h4>Earnings</h4>
             <h3 class="homedashvalue">${earnings}</h3>
-        </div>
-        <div class="col-sm-2">
         </div>
         <div class="col-sm-5 redbox">
             <h4>Expenses</h4>
