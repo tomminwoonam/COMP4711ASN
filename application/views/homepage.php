d885140996da463c66ae4cac4e5fe8fe19fb13dd
ashayegh <shayegh.software@gmail.com>
2017-02-12 16:55:55 -0800

    homepage view created

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,3 +1,29 @@
-<div class="row">
-	amir's part
+<div class="container-fluid">
+    <h2>Summary</h2>
+    <div class="col-sm-12">
+        <div class="col-sm-5 graybox">
+            <h4>Parts on hand</h4>
+            {totparts}
+        </div>
+        <div class="col-sm-2">
+        </div>
+        <div class="col-sm-5 bluebox">
+            <h4>Assembled bots</h4>
+            {totbots}
+        </div>
+    </div>
+    <div class="col-sm-12">
+    </div>
+    <div class="col-sm-12">
+        <div class="col-sm-5 greenbox">
+            <h4>Earnings</h4>
+            {earnings}
+        </div>
+        <div class="col-sm-2">
+        </div>
+        <div class="col-sm-5 redbox">
+            <h4>Expenses</h4>
+            {expenses}
+        </div>
+    </div>
 </div>
