7192535648d12ba8b43439cb210e433e80f39d1e
ashayegh <shayegh.software@gmail.com>
2017-02-13 03:26:50 -0800

    added favicon

--- application/views/template.php
+++ application/views/template.php
@@ -11,6 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="/css/template.css" />
+        <link rel="icon" href="/img/favicon.ico" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
