dd0784cba28676d88674aecf8cf01d706ca89d3f
Ntori Nyamekye <xtoribear@gmail.com>
2017-02-12 17:26:29 -0800

    adding adding cost and revenue to history model; editing history view

--- application/models/Histories.php
+++ application/models/Histories.php
@@ -8,24 +8,24 @@
  */
 class Histories extends CI_Model {
     var $data = array(
-        array('id' => '1', 'historyType' => 'Purchase', 'dateTime' => '02-01-2017 10:54'),
-        array('id' => '2', 'historyType' => 'Assembly', 'dateTime' => '02-01-2017 04:40'),
-        array('id' => '3', 'historyType' => 'Shipment', 'dateTime' => '02-01-2017 10:30'),
-        array('id' => '4', 'historyType' => 'Assembly', 'dateTime' => '02-02-2017 18:50'),
-        array('id' => '5', 'historyType' => 'Purchase', 'dateTime' => '02-02-2017 21:45'),
-        array('id' => '6', 'historyType' => 'Assembly', 'dateTime' => '02-02-2017 09:30'),
-        array('id' => '7', 'historyType' => 'Shipment', 'dateTime' => '02-03-2017 18:50'),
-        array('id' => '8', 'historyType' => 'Purchase', 'dateTime' => '02-03-2017 21:45'),
-        array('id' => '9', 'historyType' => 'Assembly', 'dateTime' => '02-03-2017 09:30'),
-        array('id' => '10', 'historyType' => 'Shipment', 'dateTime' => '02-04-2017 18:50'),
-        array('id' => '11', 'historyType' => 'Assembly', 'dateTime' => '02-04-2017 21:45'),
-        array('id' => '12', 'historyType' => 'Purchase', 'dateTime' => '02-04-2017 09:30'),
-        array('id' => '13', 'historyType' => 'Shipment', 'dateTime' => '02-05-2017 18:50'),
-        array('id' => '14', 'historyType' => 'Purchase', 'dateTime' => '02-05-2017 21:45'),
-        array('id' => '15', 'historyType' => 'Assembly', 'dateTime' => '02-05-2017 09:30'),
-        array('id' => '16', 'historyType' => 'Shipment', 'dateTime' => '02-06-2017 18:50'),
-        array('id' => '17', 'historyType' => 'Assembly', 'dateTime' => '02-06-2017 21:45'),
-        array('id' => '18', 'historyType' => 'Purchase', 'dateTime' => '02-06-2017 09:30')
+        array('id' => '1', 'historyType' => 'Purchase', 'dateTime' => '02-01-2017 10:54', 'cost' => '50', 'revenue' => 100),
+        array('id' => '2', 'historyType' => 'Assembly', 'dateTime' => '02-01-2017 04:40', 'cost' => '50', 'revenue' => 100),
+        array('id' => '3', 'historyType' => 'Shipment', 'dateTime' => '02-01-2017 10:30', 'cost' => '50', 'revenue' => 100),
+        array('id' => '4', 'historyType' => 'Assembly', 'dateTime' => '02-02-2017 18:50', 'cost' => '50', 'revenue' => 100),
+        array('id' => '5', 'historyType' => 'Purchase', 'dateTime' => '02-02-2017 21:45', 'cost' => '50', 'revenue' => 100),
+        array('id' => '6', 'historyType' => 'Assembly', 'dateTime' => '02-02-2017 09:30', 'cost' => '50', 'revenue' => 100),
+        array('id' => '7', 'historyType' => 'Shipment', 'dateTime' => '02-03-2017 18:50', 'cost' => '50', 'revenue' => 100),
+        array('id' => '8', 'historyType' => 'Purchase', 'dateTime' => '02-03-2017 21:45', 'cost' => '50', 'revenue' => 100),
+        array('id' => '9', 'historyType' => 'Assembly', 'dateTime' => '02-03-2017 09:30', 'cost' => '50', 'revenue' => 100),
+        array('id' => '10', 'historyType' => 'Shipment', 'dateTime' => '02-04-2017 18:50', 'cost' => '50', 'revenue' => 100),
+        array('id' => '11', 'historyType' => 'Assembly', 'dateTime' => '02-04-2017 21:45', 'cost' => '50', 'revenue' => 100),
+        array('id' => '12', 'historyType' => 'Purchase', 'dateTime' => '02-04-2017 09:30', 'cost' => '50', 'revenue' => 100),
+        array('id' => '13', 'historyType' => 'Shipment', 'dateTime' => '02-05-2017 18:50', 'cost' => '50', 'revenue' => 100),
+        array('id' => '14', 'historyType' => 'Purchase', 'dateTime' => '02-05-2017 21:45', 'cost' => '50', 'revenue' => 100),
+        array('id' => '15', 'historyType' => 'Assembly', 'dateTime' => '02-05-2017 09:30', 'cost' => '50', 'revenue' => 100),
+        array('id' => '16', 'historyType' => 'Shipment', 'dateTime' => '02-06-2017 18:50', 'cost' => '50', 'revenue' => 100),
+        array('id' => '17', 'historyType' => 'Assembly', 'dateTime' => '02-06-2017 21:45', 'cost' => '50', 'revenue' => 100),
+        array('id' => '18', 'historyType' => 'Purchase', 'dateTime' => '02-06-2017 09:30', 'cost' => '50', 'revenue' => 100)
     );
     // Constructor
     public function __construct()
